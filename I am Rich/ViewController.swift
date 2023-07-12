423815c1ad65a3b99afd9fc3b165caf9ca7ebd49
HEYKASHIF <kashifmohd269@gmail.com>
2023-07-13 00:30:57 +0530

    commi-2

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by HEY KASHIF on 27/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
